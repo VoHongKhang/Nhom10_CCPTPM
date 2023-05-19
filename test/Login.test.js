0298915f50a9f59f5563ba5ffeb064f9c44e8694
ducanh1811 <119037853+ducanh1811@users.noreply.github.com>
2023-05-20 01:14:31 +0700

    Update Login.test.js

--- test/Login.test.js
+++ test/Login.test.js
@@ -1,4 +1,4 @@
-const { Builder, By, Key, until } = require('selenium-webdriver');
+const { Builder, By } = require('selenium-webdriver');
 const { beforeAll, afterAll, test } = require('@jest/globals');
 
 jest.setTimeout(100000); // timeout toàn cục
@@ -13,26 +13,82 @@ afterAll(async () => {
     await driver.quit();
 });
 
-test('Login Test', async () => {
+test('Login Success', async () => {
     jest.setTimeout(100000);
     // Navigate to the login page
-    await driver.get('http://localhost:3000/login');
+    await driver.get('http://18.206.170.222/login');
 
     // Find the email and password input fields, and the submit button
-    const emailField = await driver.findElement(By.id('username'));
+    const emailField = await driver.findElement(By.id('email'));
     const passwordField = await driver.findElement(By.id('password'));
-    const submitButton = await driver.findElement(By.id('login-button'));
+    const submitButton = await driver.findElement(By.id('btn-login'));
 
     // Enter the email and password, and click the submit button
-    await emailField.sendKeys('ducanh');
-    await passwordField.sendKeys('anh181102');
+    await emailField.sendKeys('brenhernandez215@gmail.com');
+    await passwordField.sendKeys('XHh9X139h2');
     await submitButton.click();
+});
 
-    // Wait for the page to load
+test('Login Failed Email', async () => {
+    jest.setTimeout(100000);
+    // Navigate to the login page
+    await driver.get('http://18.206.170.222/login');
 
-    await driver.wait(until.urlContains('/dashboard'), 100000);
+    // Find the email and password input fields, and the submit button
+    const emailField = await driver.findElement(By.id('email'));
+    const passwordField = await driver.findElement(By.id('password'));
+    const submitButton = await driver.findElement(By.id('btn-login'));
 
-    // Check if the page contains the dashboard header
-    const header = await driver.findElement(By.css('h1.dashboard-header'));
-    expect(await header.getText()).toEqual('Dashboard');
+    // Enter the email and password, and click the submit button
+    await emailField.sendKeys('khang@gmail.com');
+    await passwordField.sendKeys('XHh9X139h2');
+    await submitButton.click();
+});
+
+test('Login Failed Password', async () => {
+    jest.setTimeout(100000);
+    // Navigate to the login page
+    await driver.get('http://18.206.170.222/login');
+
+    // Find the email and password input fields, and the submit button
+    const emailField = await driver.findElement(By.id('email'));
+    const passwordField = await driver.findElement(By.id('password'));
+    const submitButton = await driver.findElement(By.id('btn-login'));
+
+    // Enter the email and password, and click the submit button
+    await emailField.sendKeys('khang@gmail.com');
+    await passwordField.sendKeys('XHh9X139h2');
+    await submitButton.click();
+});
+
+test('Login Failed Account Does Not Exits', async () => {
+    jest.setTimeout(100000);
+    // Navigate to the login page
+    await driver.get('http://18.206.170.222/login');
+
+    // Find the email and password input fields, and the submit button
+    const emailField = await driver.findElement(By.id('email'));
+    const passwordField = await driver.findElement(By.id('password'));
+    const submitButton = await driver.findElement(By.id('btn-login'));
+
+    // Enter the email and password, and click the submit button
+    await emailField.sendKeys('khang@gmail.com');
+    await passwordField.sendKeys('XHh9X139h2');
+    await submitButton.click();
+});
+
+test('Login Failed Email Invalid', async () => {
+    jest.setTimeout(100000);
+    // Navigate to the login page
+    await driver.get('http://18.206.170.222/login');
+
+    // Find the email and password input fields, and the submit button
+    const emailField = await driver.findElement(By.id('email'));
+    const passwordField = await driver.findElement(By.id('password'));
+    const submitButton = await driver.findElement(By.id('btn-login'));
+
+    // Enter the email and password, and click the submit button
+    await emailField.sendKeys('khanggmail.com');
+    await passwordField.sendKeys('XHh9X139h2');
+    await submitButton.click();
 });
