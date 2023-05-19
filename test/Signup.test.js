fa5ab831487cd95bb0f1c680f3ce7bf82cdc0cd7
Võ Hồng Khang <87598819+VoHongKhang@users.noreply.github.com>
2023-05-20 01:28:45 +0700

    Update Signup.test.js

--- test/Signup.test.js
+++ test/Signup.test.js
@@ -16,7 +16,7 @@ afterAll(async () => {
 test('Signup Success', async () => {
     jest.setTimeout(100000);
     // Navigate to the login page
-    await driver.get('http://localhost:4000/signup');
+    await driver.get('http://18.206.170.222/signup');
 
    
     const fullname = await driver.findElement(By.id('fullname'));
@@ -33,3 +33,88 @@ test('Signup Success', async () => {
     await Confirmpassword.sendKeys('XHh9X139h2');
     await signup.click();
 });
+
+test('Signup Failed Full Name Empty', async () => {
+    jest.setTimeout(100000);
+    // Navigate to the login page
+    await driver.get('http://18.206.170.222/signup');
+
+   
+    const fullname = await driver.findElement(By.id('fullname'));
+    const phonenumber = await driver.findElement(By.id('phonenumber'));
+    const email = await driver.findElement(By.id('email'));
+    const password = await driver.findElement(By.id('password'));
+    const Confirmpassword = await driver.findElement(By.id('Confirmpassword'));
+    const signup = await driver.findElement(By.id('btn-signup'));
+
+    await fullname.sendKeys('');
+    await phonenumber.sendKeys('XHh9X139h2');
+    await email.sendKeys('brenhernandez215@gmail.com');
+    await password.sendKeys('XHh9X139h2');
+    await Confirmpassword.sendKeys('XHh9X139h2');
+    await signup.click();
+});
+
+test('Signup Failed Phone Empty', async () => {
+    jest.setTimeout(100000);
+    // Navigate to the login page
+    await driver.get('http://18.206.170.222/signup');
+
+   
+    const fullname = await driver.findElement(By.id('fullname'));
+    const phonenumber = await driver.findElement(By.id('phonenumber'));
+    const email = await driver.findElement(By.id('email'));
+    const password = await driver.findElement(By.id('password'));
+    const Confirmpassword = await driver.findElement(By.id('Confirmpassword'));
+    const signup = await driver.findElement(By.id('btn-signup'));
+
+    await fullname.sendKeys('khang@gmail.com');
+    await phonenumber.sendKeys('');
+    await email.sendKeys('brenhernandez215@gmail.com');
+    await password.sendKeys('XHh9X139h2');
+    await Confirmpassword.sendKeys('XHh9X139h2');
+    await signup.click();
+});
+
+test('Signup Failed Email Empty', async () => {
+    jest.setTimeout(100000);
+    // Navigate to the login page
+    await driver.get('http://18.206.170.222/signup');
+
+   
+    const fullname = await driver.findElement(By.id('fullname'));
+    const phonenumber = await driver.findElement(By.id('phonenumber'));
+    const email = await driver.findElement(By.id('email'));
+    const password = await driver.findElement(By.id('password'));
+    const Confirmpassword = await driver.findElement(By.id('Confirmpassword'));
+    const signup = await driver.findElement(By.id('btn-signup'));
+
+    await fullname.sendKeys('khangvohong');
+    await phonenumber.sendKeys('XHh9X139h2');
+    await email.sendKeys('');
+    await password.sendKeys('XHh9X139h2');
+    await Confirmpassword.sendKeys('XHh9X139h2');
+    await signup.click();
+});
+
+test('Signup Failed Password Empty', async () => {
+    jest.setTimeout(100000);
+    // Navigate to the login page
+    await driver.get('http://18.206.170.222/signup');
+
+   
+    const fullname = await driver.findElement(By.id('fullname'));
+    const phonenumber = await driver.findElement(By.id('phonenumber'));
+    const email = await driver.findElement(By.id('email'));
+    const password = await driver.findElement(By.id('password'));
+    const Confirmpassword = await driver.findElement(By.id('Confirmpassword'));
+    const signup = await driver.findElement(By.id('btn-signup'));
+
+    await fullname.sendKeys('khangvohong');
+    await phonenumber.sendKeys('XHh9X139h2');
+    await email.sendKeys('khang@gmail.com');
+    await password.sendKeys('');
+    await Confirmpassword.sendKeys('XHh9X139h2');
+    await signup.click();
+});
+
