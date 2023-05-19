40250e0a5770295a7e9571166e3dfb1bf211dd15
Võ Hồng Khang <87598819+VoHongKhang@users.noreply.github.com>
2023-05-20 01:21:02 +0700

    Update Logout.test.js

--- test/Logout.test.js
+++ test/Logout.test.js
@@ -16,7 +16,7 @@ afterAll(async () => {
 test('Logout Success', async () => {
     jest.setTimeout(100000);
     // Navigate to the login page
-    await driver.get('http://localhost:4000/login');
+    await driver.get('http://18.206.170.222/login');
 
     // Find the email and password input fields, and the submit button
     const emailField = await driver.findElement(By.id('email'));
@@ -30,5 +30,25 @@ test('Logout Success', async () => {
     await submitButton.click();
 
     await driver.get('http://localhost:4000/logout');
+
+});
+
+test('Logout Failed', async () => {
+    jest.setTimeout(100000);
+    // Navigate to the login page
+    await driver.get('http://18.206.170.222/login');
+
+    // Find the email and password input fields, and the submit button
+    const emailField = await driver.findElement(By.id('email'));
+    const passwordField = await driver.findElement(By.id('password'));
+    const submitButton = await driver.findElement(By.id('btn-login'));
+    
+
+    // Enter the email and password, and click the submit button
+    await emailField.sendKeys('brenhernandez215@gmail.com');
+    await passwordField.sendKeys('XHh9X139h2');
+    await submitButton.click();
+
+    await driver.get('http://18.206.170.222/logout');
 
 });
