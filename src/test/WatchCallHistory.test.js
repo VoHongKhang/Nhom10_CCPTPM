ace43c07080c0bf5cf2dc68943312a21a32283e7
Võ Hồng Khang <87598819+VoHongKhang@users.noreply.github.com>
2023-05-20 01:57:31 +0700

    Update WatchCallHistory.test.js

--- src/test/WatchCallHistory.test.js
+++ src/test/WatchCallHistory.test.js
@@ -1,1 +1,52 @@
 
+const { Builder, By } = require('selenium-webdriver');
+const { beforeAll, afterAll, test } = require('@jest/globals');
+
+jest.setTimeout(100000); // timeout toàn cục
+
+let driver;
+
+beforeAll(async () => {
+    driver = await new Builder().forBrowser('chrome').build();
+});
+
+afterAll(async () => {
+    await driver.quit();
+});
+
+test('Watch Call History Success', async () => {
+    jest.setTimeout(100000);
+    // Navigate to the login page
+    await driver.get('http://18.206.170.222/login');
+
+    // Find the email and password input fields, and the submit button
+    const emailField = await driver.findElement(By.id('email'));
+    const passwordField = await driver.findElement(By.id('password'));
+    const submitButton = await driver.findElement(By.id('btn-login'));
+
+    // Enter the email and password, and click the submit button
+    await emailField.sendKeys('brenhernandez215@gmail.com');
+    await passwordField.sendKeys('XHh9X139h2');
+
+    await driver.get('http://18.206.170.222/history/1');
+
+});
+
+test('Watch Call History Failed: Exist call history', async () => {
+    jest.setTimeout(100000);
+    // Navigate to the login page
+    await driver.get('http://18.206.170.222/login');
+
+    // Find the email and password input fields, and the submit button
+    const emailField = await driver.findElement(By.id('email'));
+    const passwordField = await driver.findElement(By.id('password'));
+    const submitButton = await driver.findElement(By.id('btn-login'));
+
+    // Enter the email and password, and click the submit button
+    await emailField.sendKeys('brenhernandez215@gmail.com');
+    await passwordField.sendKeys('XHh9X139h2');
+    await submitButton.click();
+
+    await driver.get('http://18.206.170.222/history/1000');
+
+});
